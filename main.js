db35ccfac9694b96e3acb0cadbc3be649ea379c9
Maria Lohman <marialohman1@hotmail.com>
2023-02-10 17:57:04 +0100

    added searchbox

--- main.js
+++ main.js
@@ -4,11 +4,11 @@ const searchIcon = document.getElementById("search-icon");
 const searchBox = document.getElementById("searchbox");
 
 searchIcon.addEventListener('click', function() {
-    if(searchBox.style.top == '72px'){
+    if(searchBox.style.top == '120px'){
         searchBox.style.top = '24px';
         searchBox.style.pointerEvents = 'none';
     } else {
-        searchBox.style.top = '72px';
+        searchBox.style.top = '120px';
         searchBox.style.pointerEvents = 'auto';
     }
   });
